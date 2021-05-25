d5eeb6c3f30ac25cb965d2c9903b671008c1cef7
Idilhyder <idilhyder@gmail.com>
2021-05-25 15:38:19 -0400

    First commit- structure, firebase config completed.

---
+++ src/lib/firebase.js
@@ -0,0 +1,28 @@
+import Firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+// import seed file in here
+
+// import { seedDatabase } from '../seed';
+
+// Comment out seedDatabase- do not call twice!
+
+const config = {
+  apiKey: 'AIzaSyCTFj_lDmDuQFXEKpQ6KKot4hqMpmqxmIU',
+  authDomain: 'instagram-9dee0.firebaseapp.com',
+  projectId: 'instagram-9dee0',
+  storageBucket: 'instagram-9dee0.apospory.com',
+  messagingSenderId: '430137980523',
+  appId: '1:430137980523:web:e9f3d95a19a163cd12632e'
+};
+
+const firebase = Firebase.initializeApp(config);
+const { FieldValue } = Firebase.firestore;
+
+// call import seedDatabase
+
+seedDatabase(firebase);
+
+export { firebase, FieldValue };
+
